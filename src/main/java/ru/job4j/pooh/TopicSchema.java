2e007d493e4af886f3cf556a3f3866c3fac620ee
itlazykin <dzaka89@gmail.com>
2024-12-13 09:36:56 +0300

    init commit

---
+++ src/main/java/ru/job4j/pooh/TopicSchema.java
@@ -0,0 +1,18 @@
+package ru.job4j.pooh;
+
+public class TopicSchema implements Schema {
+    @Override
+    public void addReceiver(Receiver receiver) {
+
+    }
+
+    @Override
+    public void publish(Message message) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
